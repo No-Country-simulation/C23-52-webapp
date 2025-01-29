e45b8c357f7a5d8fb94879bf5bfa60def19e4d81
Retford <retfordjb@gmail.com>
2025-01-29 15:57:52 -0500

    auth0 added

--- frontend/ruminet/src/app/layout.js
+++ frontend/ruminet/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/store/Providers';
+import Auth0Provider from '@/auth0/components/Auth0Provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -19,12 +20,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='es'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <Auth0Provider>
+      <html lang='es'>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    </Auth0Provider>
   );
 }

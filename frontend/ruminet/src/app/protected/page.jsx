dcfe556d85a15d407ce05d88715093bd57a1d6cc
Lichu23 <lisandroarenas@outlook.com>
2025-01-29 21:08:33 -0300

    fix errors

--- frontend/ruminet/src/app/protected/page.jsx
+++ frontend/ruminet/src/app/protected/page.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+"use client"
+import { useUser } from "@auth0/nextjs-auth0/client";
+import React from "react";
 
 export default function Protected() {
-  return (
-    <div>Protected</div>
-  )
+  const { user } = useUser();
+
+  if (!user) {
+    console.log("Tenes que logearte");
+  } else {
+    return <div>Protected</div>;
+  }
 }

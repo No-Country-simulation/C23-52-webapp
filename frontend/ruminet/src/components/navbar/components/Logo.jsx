c9066f055fe9e9dfc0270dfdf0f7ff4c0ad160a7
Lichu23 <lisandroarenas@outlook.com>
2025-01-30 21:53:00 -0300

    change logo and titles in navbar links

--- frontend/ruminet/src/components/navbar/components/Logo.jsx
+++ frontend/ruminet/src/components/navbar/components/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <div className="md:ml-10  md:mr-10 ml-16">
       
-      <Image src="/images/logo-ruminet.png" className="object-cover" alt="logo-ruminet" width={200} height={100}/>
+      <Image src="/images/LogoNavbar.png" className="object-cover" alt="logo-ruminet" width={200} height={100}/>
     </div>
   );
 }
